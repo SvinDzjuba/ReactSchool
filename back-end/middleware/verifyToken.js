1ff50eb314e6f9b29a7c86fe3e7be0211601e045
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-05-26 09:07:16 +0300

    Update verifyToken.js

--- back-end/middleware/verifyToken.js
+++ back-end/middleware/verifyToken.js
@@ -5,7 +5,7 @@ export const verifyToken = (req, res, next) => {
     const accessToken = authHeader && authHeader.split(' ')[1];
 
     if (!accessToken) return res.status(401).json({ message: 'User not authenticated!' });
-    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) return res.status(403).json({ message: 'Invalid token!' });
         req.email = decoded.email;
         next();
